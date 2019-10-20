d3ff41431df0c81afa7b994474c6992054f615c7
taiichi.igawa <ihciiat@gmail.com>
2019-10-21 07:31:05 +0900

    フォーマット

--- src/com/tigawa/util/AboutMap.java
+++ src/com/tigawa/util/AboutMap.java
@@ -4,52 +4,69 @@ import java.util.Map;
 import java.util.TreeMap;
 
 /**
- * {@code TreeMap}を継承したクラス
- * キーが一致しない場合は直近の小さい（または大きい）値のキーに一致するValueを返します。
+ * {@code TreeMap}を継承したクラス キーが一致しない場合は直近の小さい（または大きい）値のキーに一致するValueを返します。
  * 
- * <p>重要</p>
+ * <p>
+ * 重要
+ * </p>
+ * 
  * <pre>
  * + キーはNumberを継承しているクラスしか指定できません。(Integer, Long, Float...)
  * + getメソッドしかオーバライドしていません。それ以外はTreeMapと同じ動作をします。
  * </pre>
  *
- * <p>キーが一致しない場合に、より小さい方のキーに一致する値を返します。</p>
+ * <p>
+ * キーが一致しない場合に、より小さい方のキーに一致する値を返します。
+ * </p>
+ * 
  * <pre>
- * {@code
- *  AboutMap<Integer, String> aboutMap = new AboutMap<Integer, String>() {{
- *    put(2019, "A");
- *    put(2021, "B");
- *  }};
- *  aboutMap.get(2018); //=> null
- *  aboutMap.get(2019); //=> "A"
- *  aboutMap.get(2020); //=> "A"
- *  aboutMap.get(2021); //=> "B"
- *  aboutMap.get(2022); //=> "B"
+ * {
+ * 	&#064;code
+ * 	AboutMap&lt;Integer, String&gt; aboutMap = new AboutMap&lt;Integer, String&gt;() {
+ * 		{
+ * 			put(2019, &quot;A&quot;);
+ * 			put(2021, &quot;B&quot;);
+ * 		}
+ * 	};
+ * 	aboutMap.get(2018); // =&gt; null
+ * 	aboutMap.get(2019); // =&gt; &quot;A&quot;
+ * 	aboutMap.get(2020); // =&gt; &quot;A&quot;
+ * 	aboutMap.get(2021); // =&gt; &quot;B&quot;
+ * 	aboutMap.get(2022); // =&gt; &quot;B&quot;
  * }
  * </pre>
  *
- * <p>キーが一致しない場合に、より大きい方のキーに一致する値を返します。</p>
+ * <p>
+ * キーが一致しない場合に、より大きい方のキーに一致する値を返します。
+ * </p>
+ * 
  * <pre>
- * {@code
- *  boolean descending = true;
- *  AboutMap<Integer, String> aboutMap = new AboutMap<Integer, String>(descending) {{
- *    put(2019, "A");
- *    put(2021, "B");
- *  }};
- *  aboutMap.get(2018); //=> "A"
- *  aboutMap.get(2019); //=> "A"
- *  aboutMap.get(2020); //=> "B"
- *  aboutMap.get(2021); //=> "B"
- *  aboutMap.get(2022); //=> null
+ * {
+ * 	&#064;code
+ * 	boolean descending = true;
+ * 	AboutMap&lt;Integer, String&gt; aboutMap = new AboutMap&lt;Integer, String&gt;(
+ * 			descending) {
+ * 		{
+ * 			put(2019, &quot;A&quot;);
+ * 			put(2021, &quot;B&quot;);
+ * 		}
+ * 	};
+ * 	aboutMap.get(2018); // =&gt; &quot;A&quot;
+ * 	aboutMap.get(2019); // =&gt; &quot;A&quot;
+ * 	aboutMap.get(2020); // =&gt; &quot;B&quot;
+ * 	aboutMap.get(2021); // =&gt; &quot;B&quot;
+ * 	aboutMap.get(2022); // =&gt; null
  * }
  * </pre>
  * 
- * @param <K> 数値型である必要があります。
- * @param <V> the type of mapped values
+ * @param <K>
+ *            数値型である必要があります。
+ * @param <V>
+ *            the type of mapped values
  * 
  * @author igawa.taiichi
  * @see TreeMap
- * @version  1.0
+ * @version 1.0
  */
 public class AboutMap<K extends Number, V> extends TreeMap<K, V> {
 	private static final long serialVersionUID = 1518115962300131292L;
@@ -65,7 +82,9 @@ public class AboutMap<K extends Number, V> extends TreeMap<K, V> {
 
 	/**
 	 * コンストラクタ 空のTreeマップ
-	 * @param descending　true:降順  false:昇順 (default)
+	 * 
+	 * @param descending
+	 *            　true:降順 false:昇順 (default)
 	 */
 	public AboutMap(boolean descending) {
 		super();
@@ -74,29 +93,31 @@ public class AboutMap<K extends Number, V> extends TreeMap<K, V> {
 
 	/**
 	 * キーが一致しない場合は直近の小さい（または大きい）値のキーに一致するValueを返します。
-	 * @param key キー（数値型）
+	 * 
+	 * @param key
+	 *            キー（数値型）
 	 * @return 値
 	 */
 	@Override
 	public V get(Object key) {
 		if (this.containsKey(key))
 			return super.get(key);
-		
+
 		if (key instanceof Number == false)
 			throw new IllegalArgumentException("Key is not Number");
-		
-		Number nKey = ((Number)key);
+
+		Number nKey = ((Number) key);
 
 		Map<K, V> map = descending 
-						? this
+						? this 
 						: this.descendingMap();
-						
-	    for (K k : map.keySet()) {
-	    	if(descending 
-				? k.doubleValue() > nKey.doubleValue()
-				: k.doubleValue() < nKey.doubleValue())
-	    		return super.get(k);
-	    }
+
+		for (K k : map.keySet()) {
+			if (descending 
+					? k.doubleValue() > nKey.doubleValue() 
+					: k.doubleValue() < nKey.doubleValue())
+				return super.get(k);
+		}
 		return null;
-	}	
+	}
 }
